7a4893359ebdce3e1cca2fdfd921d79503f4c09b
whitneymichelle <wmmarks10@gmail.com>
2020-12-03 21:31:06 -0600

    modified plumber file

--- plumber_file.R
+++ plumber_file.R
@@ -10,21 +10,5 @@ prepped_recipe <- model$pre$actions$recipe$recipe %>% prep()
 #* @param qsec
 #* @param am
 #* @post /predict
-CalculatePrediction <- function(df){
-  
-inputted_data_processed <- bake(prepped_recipe, new_data = df, all_predictors())
-  
-fraud_predict <- predict(fit, df, type = "prob") %>% 
-  bind_cols(test_df %>% select_all()) 
 
-test_results <- 
-  test_results %>%
-  rename(`random forest` = .pred) %>%
-  bind_cols(
-    predict(glmn_fit, new_data = test_normalized) %>%
-      rename(glmnet = .pred)
-  )
-  
-  return(y.pred)
-}
 
